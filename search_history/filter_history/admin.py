0ac6e34615a7d693a44891909abd17dd2de4ef43
nipulkm <nipulkm@gmail.com>
2022-02-24 21:43:19 +0600

    Model Implemented

--- search_history/filter_history/admin.py
+++ search_history/filter_history/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from filter_history.models import User, Keyword, Result, History
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_ip_address']
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'word']
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'search_result', 'keyword']
+
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'time', 'date', 'user', 'keyword']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Keyword, KeywordAdmin)
+admin.site.register(Result, ResultAdmin)
+admin.site.register(History, HistoryAdmin)
--- search_history/filter_history/models.py
+++ search_history/filter_history/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_ip_address = models.CharField(max_length=39, unique=True)
+
+class Keyword(models.Model):
+    word = models.CharField(max_length=255, unique=True)
+
+class Result(models.Model):
+    search_result = models.CharField(max_length=255, unique=True)
+    keyword = models.ForeignKey(Keyword, on_delete=models.PROTECT)
+
+class History(models.Model):
+    time = models.TimeField(auto_now=True, auto_now_add=False)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    keyword = models.ForeignKey(Keyword, on_delete=models.PROTECT)
